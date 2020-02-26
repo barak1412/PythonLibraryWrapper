ea59b180d2b9444a48f2bc03906ee3597fa89e22
barak1412 <barak1413@gmail.com>
2020-02-26 08:49:29 +0200

    Fixed typos

--- library_lib/wrapper/lib_wrapper.py
+++ library_lib/wrapper/lib_wrapper.py
@@ -1,10 +1,11 @@
 import os
-from ctypes.util import find_libraty
+from ctypes.util import find_library
 from ctypes import cdll
 
 os.environ['PATH'] += 'C:\PythonLib;'
-lib_dll_path = find_libraty('ExampleDLL')
-dll_handle = cdll.LoadLibray(lib_dll_path)
+lib_dll_path = find_library('ExampleDLL')
+dll_handle = cdll.LoadLibrary(lib_dll_path)
+print(dll_handle)
 
 class LibWrapper(object):
 	@staticmethod
